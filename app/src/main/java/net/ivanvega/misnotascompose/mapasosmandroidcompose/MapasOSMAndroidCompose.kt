3ef8957435c9fd40b10c5dfab7eabcf6febab4df
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2023-11-14 14:57:19 -0600

    Se agregaron marcadores y polilineas

--- app/src/main/java/net/ivanvega/misnotascompose/mapasosmandroidcompose/MapasOSMAndroidCompose.kt
+++ app/src/main/java/net/ivanvega/misnotascompose/mapasosmandroidcompose/MapasOSMAndroidCompose.kt
@@ -1,20 +1,33 @@
 package net.ivanvega.misnotascompose.mapasosmandroidcompose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.utsman.osmandcompose.CameraState
 import com.utsman.osmandcompose.DefaultMapProperties
+import com.utsman.osmandcompose.Marker
 import com.utsman.osmandcompose.OpenStreetMap
+import com.utsman.osmandcompose.Polyline
 import com.utsman.osmandcompose.ZoomButtonVisibility
 import com.utsman.osmandcompose.rememberCameraState
+import com.utsman.osmandcompose.rememberMarkerState
 import com.utsman.osmandcompose.rememberOverlayManagerState
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
@@ -29,13 +42,20 @@ fun OSMComposeMapa(
         mutableStateOf(DefaultMapProperties)
     }
 
+    // define marker state
+    val depokMarkerState = rememberMarkerState(
+        geoPoint = GeoPoint(20.1389,-101.15088),
+        rotation = 90f // default is 0f
+    )
+
+
     // setup mapProperties in side effect
     SideEffect {
         mapProperties = mapProperties
             .copy(isTilesScaledToDpi = true)
             .copy(tileSources = TileSourceFactory.MAPNIK)
             .copy(isEnableRotationGesture = true)
-            .copy(zoomButtonVisibility = ZoomButtonVisibility.NEVER)
+            .copy(zoomButtonVisibility = ZoomButtonVisibility.SHOW_AND_FADEOUT)
     }
 
 
@@ -44,6 +64,16 @@ fun OSMComposeMapa(
         geoPoint = GeoPoint(20.1389,-101.15088)
         zoom = 18.0 // optional, default is 5.0
     }
+
+    // define polyline
+    val geoPointPoluLyne = remember {
+        listOf(GeoPoint(20.1389,-101.15088),
+            GeoPoint(20.1434,-101.1498),
+            GeoPoint(20.14387,-101.15099),
+            GeoPoint(20.14395,-101.15128),
+            GeoPoint(20.14411,-101.15186))
+    }
+
 
     val overlayManagerState = rememberOverlayManagerState()
 
@@ -56,8 +86,26 @@ fun OSMComposeMapa(
             val copyright = CopyrightOverlay(ctx)
             overlayManagerState.overlayManager.add(copyright) // add another overlay in this listener
         },
-        modifier = modifier)
+        modifier = modifier
+    )
     {
+        Marker(state = depokMarkerState, title="ITSUR", snippet = "Escuela") {
+            // create info window node
+            Column(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(color = Color.Gray, shape = RoundedCornerShape(7.dp)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // setup content of info window
+                Text(text = it.title)
+                Text(text = it.snippet, fontSize = 10.sp)
+            }
 
+        }
+        Polyline(geoPoints = geoPointPoluLyne) {
+
+        }
     }
 }
